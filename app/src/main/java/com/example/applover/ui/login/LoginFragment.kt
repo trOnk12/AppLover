2279c509cc09e2b3f1d7c34e082af756fc0042e0
mateusz.pachulski <mateusz.pachulski@famoc.com>
2020-04-03 22:05:58 +0200

    added navigation component logic

---
+++ app/src/main/java/com/example/applover/ui/login/LoginFragment.kt
@@ -0,0 +1,10 @@
+package com.example.applover.ui.login
+
+import androidx.fragment.app.Fragment
+import com.example.applover.R
+
+class LoginFragment : Fragment(R.layout.login_fragment) {
+
+
+
+}
---
+++ app/src/main/java/com/example/applover/ui/login/StatusFragment.kt
@@ -0,0 +1,8 @@
+package com.example.applover.ui.login
+
+import androidx.fragment.app.Fragment
+import com.example.applover.R
+
+class StatusFragment : Fragment(R.layout.status_fragment) {
+
+}
