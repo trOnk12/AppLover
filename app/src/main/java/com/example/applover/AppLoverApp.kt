eedf518c2cca720881fd7cdc1547dbc0787d65b5
mateusz.pachulski <mateusz.pachulski@famoc.com>
2020-04-03 22:19:43 +0200

    added coreComponent to Application object

--- app/src/main/java/com/example/applover/AppLoverApp.kt
+++ app/src/main/java/com/example/applover/AppLoverApp.kt
@@ -1,8 +1,30 @@
 package com.example.applover
 
 import android.app.Application
+import android.content.Context
+import com.example.applover.di.component.CoreComponent
+import com.example.applover.di.module.ContextModule
 
-class AppLoverApp : Application(){
-    
+class AppLoverApp : Application() {
+
+    lateinit var coreComponent: CoreComponent
+
+    companion object {
+        fun coreComponent(context: Context) =
+            (context.applicationContext as AppLoverApp).coreComponent
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        initDaggerCoreComponent()
+    }
+
+    private fun initDaggerCoreComponent() {
+        coreComponent = DaggerCoreComponent
+            .builder()
+            .contextModule(ContextModule(this))
+            .build()
+    }
+
 
 }
